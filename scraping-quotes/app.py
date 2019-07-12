5485e816414736df71d0b297f69aa0db1491b8e4
CrazyDi <katuhis@yandex.ru>
2019-07-12 13:23:58 +0300

    regular

--- scraping-quotes/app.py
+++ scraping-quotes/app.py
@@ -1,9 +1,17 @@
-import requests
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError
+from selenium import webdriver
 
-from pages.quotes_page import QuotesPage
+try:
+    author = input("Enter the author you'd like quotes from: ")
+    tag = input("Enter your tag: ")
 
-page_content = requests.get('http://quotes.toscrape.com/').content
-page = QuotesPage(page_content)
+    chrome = webdriver.Chrome(executable_path="d:/chromedriver.exe")
+    chrome.get("http://quotes.toscrape.com/search.aspx")
+    page = QuotesPage(chrome)
 
-for quote in page.quotes:
-    print(quote.content)
+    print(page.search_for_quotes(author, tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error occured. Please try again.")
