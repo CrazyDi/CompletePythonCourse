72a4a9ead5ec0081b2de8f9796bd8d668f6c8d66
CrazyDi <katuhis@yandex.ru>
2019-07-12 17:34:24 +0300

    regular

--- scraping-books/app.py
+++ scraping-books/app.py
@@ -1,3 +1,7 @@
+import asyncio
+import aiohttp
+import async_timeout
+import time
 import requests
 import logging
 
@@ -15,15 +19,36 @@ logger.info('Loading books list...')
 page_content = requests.get(f'http://books.toscrape.com').content
 page = BooksPage(page_content)
 
-books = None
-for i in range(page.page_count):
-    page_content = requests.get(f'http://books.toscrape.com/catalogue/page-{i+1}.html').content
-    page = BooksPage(page_content)
-    logger.debug('Creating AllBooksPAge from page content.')
-    if books is None:
-        books = page.books
-    else:
-        books.extend(page.books)
+loop = asyncio.get_event_loop()
 
+books = page.books
+
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f'Page took {time.time() - page_start}')
+            return await response.text()
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+
+urls = [f'http://books.toscrape.com/catalogue/page-{page_num+1}.html' for page_num in range(1, page.page_count)]
+start = time.time()
+pages = loop.run_until_complete(get_multiple_pages(loop, *urls))
+print(f'Total page requests took {time.time() - start}')
+
+for page_content in pages:
+    logger.debug('Creating AllBooksPage from page content.')
+    page = BooksPage(page_content)
+    books.extend(page.books)
 
 print(len(books))
